f4ec19d695636a051e0837429b58c33cde606df5
rosefatim <rosecoulibaly30@gmail.com>
2022-08-11 16:46:12 +0000

    ok

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from "./components/tabs/Login";
 import Error from "./components/tabs/Error";
 
 
-<<<<<<< HEAD
+
 export class App extends Component {
   render() {
     return (
@@ -17,15 +17,5 @@ export class App extends Component {
         </Routes>
       </div>
     );
-  }
-=======
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="home" element={<Home />} />
-      <Route path="*" element={<>404 not found</>} />
-    </Routes>
-  );
->>>>>>> 2c0118a4d258f32e0a871655d0443b38b99ba682
+}
 }
